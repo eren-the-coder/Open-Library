73219846ccd1d0b71b5a4bb4964576a7828effa2
eren-the-coder <jeanpc568@gmail.com>
2025-10-19 11:59:40 +0100

    feat: get file url on fetch posts

--- backend/api/getPosts.php
+++ backend/api/getPosts.php
@@ -7,7 +7,14 @@
   $result = $conn->query("SELECT * FROM ressources ORDER BY id DESC");
 
   $posts = [];
+  $baseUrl = "http://127.0.0.1/backend/uploads/";
+
   while ($row = $result->fetch_assoc()) {
+      if (!empty($row['file'])) {
+          $row['fileUrl'] = $baseUrl . $row['file'];
+      } else {
+          $row['fileUrl'] = null;
+      }
       $posts[] = $row;
   }
 

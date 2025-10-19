0a18a2d00cef12f3ccef9a76fdf2e77a78ce8231
eren-the-coder <jeanpc568@gmail.com>
2025-10-19 17:27:06 +0100

    feat: use NavLink in footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,13 +1,29 @@
 import styles from './Footer.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>&copy; Mangone Messi 2025 - Tous droits réservés</p>
       <div className={styles.links}>
-        <a href="about.html" className={styles.link}>À propos</a>
-        <a href="contact.html" className={styles.link}>Me contacter</a>
-        <a href="privacy.html" className={styles.link}>Confidentialité</a>
+        <NavLink
+          className={({ isActive }) => 
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+          to="/about"
+        >À propos</NavLink>
+        <NavLink
+          className={({ isActive }) => 
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+          to="/contact"
+        >Me contacter</NavLink>
+        {/* <NavLink
+          className={({ isActive }) => 
+            isActive ? `${styles.link} ${styles.active}` : styles.link
+          }
+          to="/privacy"
+        >Confidentialité</NavLink> */}
       </div>
     </footer>
   );

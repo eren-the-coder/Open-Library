834bad625bd09abbaf6f4743115dde5832f24e2c
eren-the-coder <jeanpc568@gmail.com>
2025-10-30 21:35:12 +0100

    refactor: define database connection infos based on the dev mode

--- backend/api/config.php
+++ backend/api/config.php
@@ -1,13 +1,25 @@
 <?php
-  // $host = "localhost";
-  // $user = "root";
-  // $pass = ""; // XAMPP par défaut n’a pas de mot de passe root
-  // $dbname = "openlibrary";
 
-  $host = $config['host'];
-  $user = $config['user'];
-  $pass = $config['pass'];
-  $dbname = $config['dbname'];
+  $config = include __DIR__ . '/.env.php';
+
+  if (!is_array($config)) {
+      die(json_encode([
+          "success" => false,
+          "error" => "Le fichier .env.php ne retourne pas de configuration valide."
+      ]));
+  }
+
+  if ($config['mode'] === 'dev') {
+      $host = "localhost";
+      $user = "root";
+      $pass = "";
+      $dbname = "openlibrary";
+  } else {
+      $host = $config['host'];
+      $user = $config['user'];
+      $pass = $config['pass'];
+      $dbname = $config['dbname'];
+  }
 
   $conn = new mysqli($host, $user, $pass, $dbname);
 
@@ -17,6 +29,7 @@
           "error" => "Échec de la connexion à la base de données : " . $conn->connect_error
       ]));
   }
-  // Définir le jeu de caractères UTF-8
+
+  // Encodage UTF-8
   $conn->set_charset("utf8");
 ?>
